866e31929213a1263d105b0a1c3b317e80dd14ee
Quentindeve <quentindutilleul@pm.me>
2023-10-05 01:27:03 +0200

    Removed 2 useless files

--- styles.py
+++
@@ -1,9 +0,0 @@
-import os
-
-files = os.walk("./css")
-
-for file in files:
-    (dirname, _, filenames) = file
-
-    for filename in filenames:
-        print(f'<link rel="stylesheet" href="{dirname}/{filename}" />')
