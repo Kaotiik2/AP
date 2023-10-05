a75b62214c57ddb1464522304151a7b43a4dd437
Quentindeve <quentindutilleul@pm.me>
2023-10-05 14:05:47 +0200

    Moved files

--- static/js/forms_checks/secretaire/form-1-check.js
+++
@@ -1,6 +0,0 @@
-let form2 = document.querySelector("#pre-admission-form-1");
-
-form2.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const postal_code_input = document.querySelector("input[name=code_postal]");
-})
