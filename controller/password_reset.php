bb07d2d087f01ffbbc7a64448f9337c62285f2d3
Quentindeve <quentindutilleul@pm.me>
2023-10-05 14:08:58 +0200

    Moved password_reset update to controller to match MVC architecture.

---
+++ controller/password_reset.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("../lib/captcha.php");
+require_once("../model/users.php");
+
+session_start();
+
+$user = unserialize($_SESSION["user"]);
+// Captcha fail
+if (!captcha_verify($_POST["captcha_answer"])) {
+    header("Location: /views/password_reset.php?error=5");
+}
+// Captcha success
+else {
+    extract($_POST);
+    var_dump($user);
+
+    if (isset($submit) && $new_password == $password_repeat) {
+        // New password cannot be equals to the old one.
+        if ($new_password == get_user_password($user->$id_user)) {
+            header("Location: /views/password_reset.php?error=10");
+        }
+
+        update_user_password($user->$user_id, $new_password);
+        session_destroy();
+        header("Location: login.php");
+    }
+}
+
+function cast($value): User
+{
+    return $value;
+}
--- views/password_reset.php
+++ views/password_reset.php
@@ -5,63 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LPFS - Réinitialisation du mot de passe</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/password_reset.css" />
-    <link rel="stylesheet" href="login.css" />
-    <script src="./js/password_requirements.js" defer></script>
+    <link rel="stylesheet" href="/static/css/style.css" />
+    <link rel="stylesheet" href="/static/css/password_reset.css" />
+    <link rel="stylesheet" href="/static/css/login.css" />
+    <script src="/static/js/password_requirements.js" defer></script>
 </head>
 
 <body>
-    <?php
-    require_once("lib/captcha.php");
-    session_start();
-
-    if (!captcha_verify($_POST["captcha_answer"])) {
-        header("Location: password_reset.php?erreur=");
-    }
-
-    if (isset($_SESSION["erreur_ancien_mdp"])) {
-        echo "<p>Le nouveau mot de passe ne doit pas être le même que le nouveau</p>";
-    } else {
-        extract($_POST);
-        if (isset($submit) && $new_password == $password_repeat) {
-            require_once("lib/config.php");
-            $account_reset = $_SESSION["reset_account_id"];
-
-            $stmt = $db->prepare("SELECT mot_de_passe from utilisateurs WHERE id=:id");
-            $stmt->bindValue(":id", $account_reset);
-            $stmt->execute() or die("Erreur bdd.");
-            $old_password = $stmt->fetchAll()[0]["mot_de_passe"];
-
-            if ($new_password == $old_password) {
-                $_SESSION["erreur_ancien_mdp"] = "1";
-                session_write_close();
-                header("Location: password_reset.php");
-                die();
-            }
-
-            $req = "
-        UPDATE utilisateurs
-            SET mot_de_passe = :new_password,
-            premiere_connexion = '1',
-            date_mdp = :date
-            WHERE id = :id
-        ";
-
-            session_destroy();
-            $datenow = time();
-
-            $stmt = $db->prepare($req);
-            $stmt->bindValue(":new_password", $new_password);
-            $stmt->bindValue(":date", $datenow);
-            $stmt->bindValue(":id", $account_reset);
-            $result = $stmt->execute() or die("Impossible d'update la base de données, veuillez contacter l'administrateur.");
-
-            header("Location: login.php");
-        }
-    }
-    ?>
-
     <div id="mainscreen">
         <div class="card">
             <div class="leftside">
@@ -75,7 +25,7 @@
                     <li id="numbers-p" class="invalid"> Chiffres </li>
                 </ul>
             </div>
-            <form action="password_reset.php" method="post" id="password-reset" class="rightside login-box">
+            <form action="/controller/password_reset.php" method="post" id="password-reset" class="rightside login-box">
                 <div class="password">
                     <label for="new_password">Nouveau mot de passe</label>
                     <input type="password" name="new_password" />
@@ -86,8 +36,8 @@
                 </div>
 
                 <div class="password">
-                    <img src="gen_captcha.php" />
-                    <input type="text" id="captcha_answer" />
+                    <img src="/gen_captcha.php" />
+                    <input type="text" name="captcha_answer" />
                 </div>
                 <input type="submit" name="submit" value="Changer le mot de passe" class="submit-disabled" id="submit-button" disabled />
             </form>
