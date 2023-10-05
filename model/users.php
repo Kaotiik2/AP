101dd252bb92208eab7162257fffd2b32bbbe7c2
Quentindeve <quentindutilleul@pm.me>
2023-10-05 14:05:07 +0200

    Fixed User class construction and added a password equality function.

--- model/users.php
+++ model/users.php
@@ -37,7 +37,7 @@ function login(string $mail, string $password): User|int
         $hcb = $response[0]["premiere_connexion"] == '1';
         $mcp = diff_in_days($response[0]["date_mdp"]) >= PASSWORD_LIFE;
         $user = new User();
-        $user->$mail = $mail;
+        $user->mail = $mail;
         $user->id_user = $id_user;
         $user->id_role = $id_role;
         $user->has_connected_before = $hcb;
@@ -60,4 +60,40 @@ function diff_in_days($date)
     $datediff = $now - $date;
 
     return round($datediff / (60 * 60 * 24));
+}
+
+function get_user_password(int $user_id): string|null
+{
+    include("../lib/config.php");
+
+    $req = "SELECT mot_de_passe FROM utilisateurs WHERE id = :id";
+
+    $stmt = $db->prepare($req);
+    $stmt->bindValue(":id", $user_id);
+    $result = $stmt->execute();
+
+    if (!$result) return null;
+
+    else return $stmt->fetch()["mot_de_passe"];
+}
+
+function update_user_password(int $user_id, string $new_password): bool
+{
+    include("../lib/config.php");
+
+    $req = "
+        UPDATE utilisateurs
+            SET mot_de_passe = :new_password,
+            premiere_connexion = '1',
+            date_mdp = :date
+            WHERE id = :id
+        ";
+
+    $datenow = time();
+    $stmt = $db->prepare($req);
+    $stmt->bindValue(":new_password", $new_password);
+    $stmt->bindValue(":date", $datenow);
+    $stmt->bindValue(":id", $user_id);
+
+    return $stmt->execute();
 }
