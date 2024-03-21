f76e30fd9dc2b5598f2dcf1126457a3e0e0cb6bb
Kaotiik2 <duribreuxjoshua1@gmail.com>
2024-03-21 08:27:09 +0100

    re

--- views/login.php
+++ views/login.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title>LOGIN PAGE LPF</title>
-    <link href="/static/css/login.css" rel="stylesheet"/>
     <script src="/static/js/login.js" defer></script>
     <!-- Inclure Tailwind CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
