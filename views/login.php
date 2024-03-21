f7bbeea2f1d6d922c6aac32f328f5f50e153587a
Kaotiik2 <duribreuxjoshua1@gmail.com>
2024-03-21 15:17:41 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -35,10 +35,6 @@
                             <input class="pas border rounded p-2 w-full" type="password" name="password" placeholder="Votre Mot De Passe">
                         </div>
                     </div>
-                    <img src="/gen_captcha.php" class="mb-4" /> <br>
-                    <label for="captcha_answer" class="hidden"></label>
-                    <input type="text" name="captcha_answer" class="input_1 border rounded p-2 mb-4 w-full" placeholder="Captcha" /> <br>
-                    <button type="submit" class="button bg-blue-500 text-white py-2 px-4 rounded">Connexion</button>
                 </div>
                 <img src="/gen_captcha.php" class="mb-1 m-auto"/> <br>
                 <label for="captcha_answer" class="hidden"></label>
