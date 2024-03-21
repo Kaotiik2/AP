2234619605d6a1f9c4c3ec22a9615d3e18093f23
Quentindeve <quentindutilleul@pm.me>
2024-03-21 10:46:38 +0100

    Views/login: Idk it works

--- views/login.php
+++ views/login.php
@@ -2,50 +2,50 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>LOGIN PAGE LPF</title>
-    <link href="/static/css/login.css" rel="stylesheet"/>
-    <link href="/static/css/style.css" rel="stylesheet"/>
-    <link href="/static/css/admin.css" rel="stylesheet"/>
+    <link href="/static/css/login.css" rel="stylesheet" />
+    <link href="/static/css/style.css" rel="stylesheet" />
+    <link href="/static/css/admin.css" rel="stylesheet" />
     <script src="/static/js/login.js" defer></script>
     <!-- Inclure Tailwind CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 
 <body class="bg-gray-200">
-<div class="flex justify-center items-center h-screen">
-    <div class="max-w-md w-full bg-white p-8 rounded-md shadow-md">
-        <div class="flex justify-center items-center mb-8">
-            <img src="/static/images/LPFS_logo.png" alt="" class="w-64">
-        </div>
-        <form action="/controller/login.php" method="post" id="login-form">
-            <h1 class="text-2xl font-bold mb-4">Se Connecter</h1>
-            <div class="login-box">
-                <p id="error-display" class="text-red-500 mb-4"></p>
-                <div class="email mb-4">
-                    <label for="email" class="hidden"></label>
-                    <div class="flex items-center">
-                        <ion-icon name="at-circle-outline" class="mr-2"></ion-icon>
-                        <input type="email" name="username" placeholder="Email" class="border rounded p-2 w-full">
-                    </div>
-                </div>
-                <div class="password mb-4">
-                    <label for="password" class="hidden"></label>
-                    <div class="flex items-center">
-                        <ion-icon name="lock-closed-outline" class="mr-2"></ion-icon>
-                        <input class="pas border rounded p-2 w-full" type="password" name="password" placeholder="Votre Mot De Passe">
-                    </div>
-                </div>
-                <img src="/gen_captcha.php" class="mb-4"/> <br>
-                <label for="captcha_answer" class="hidden"></label>
-                <input type="text" name="captcha_answer" class="input_1 border rounded p-2 mb-4 w-full"/> <br>
-                <button type="submit" class="button bg-blue-500 text-white py-2 px-4 rounded">Connexion</button>
+    <div class="flex justify-center items-center h-screen">
+        <div class="max-w-md w-full bg-white p-8 rounded-md shadow-md">
+            <div class="flex justify-center items-center mb-8">
+                <img src="/static/images/LPFS_logo.png" alt="" class="w-64">
             </div>
-        </form>
+            <form action="/controller/login.php" method="post" id="login-form">
+                <h1 class="text-2xl font-bold mb-4">Se Connecter</h1>
+                <div class="login-box">
+                    <p id="error-display" class="text-red-500 mb-4"></p>
+                    <div class="email mb-4">
+                        <label for="email" class="hidden"></label>
+                        <div class="flex items-center">
+                            <ion-icon name="at-circle-outline" class="mr-2"></ion-icon>
+                            <input type="email" name="username" placeholder="Email" class="border rounded p-2 w-full">
+                        </div>
+                    </div>
+                    <div class="password mb-4">
+                        <label for="password" class="hidden"></label>
+                        <div class="flex items-center">
+                            <ion-icon name="lock-closed-outline" class="mr-2"></ion-icon>
+                            <input class="pas border rounded p-2 w-full" type="password" name="password" placeholder="Votre Mot De Passe">
+                        </div>
+                    </div>
+                    <img src="/gen_captcha.php" class="mb-4" /> <br>
+                    <label for="captcha_answer" class="hidden"></label>
+                    <input type="text" name="captcha_answer" class="input_1 border rounded p-2 mb-4 w-full" placeholder="Captcha" /> <br>
+                    <button type="submit" class="button bg-blue-500 text-white py-2 px-4 rounded">Connexion</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 </body>
 
 </html>
