3012bf40a8bc7f044cae17299bc37512c5fb371c
Kaotiik2 <duribreuxjoshua1@gmail.com>
2024-03-21 16:30:28 +0100

    Update pre_admission_result.php

--- views/admission/pre_admission_result.php
+++ views/admission/pre_admission_result.php
@@ -13,7 +13,7 @@
     <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
         <p id="success_message" class="hidden text-green-500 font-bold">Enregistrement réussi !</p>
         <p id="error_message" class="hidden text-red-500 font-bold">Échec de l'enregistrement: <?php echo $_GET["error"]; ?></p>
-        <a href="/controller/panel.php" class="text-blue-500 underline">Retourner à l'accueil</a>
+        <a href="../../index.html" class="text-blue-500 underline">Retourner à l'accueil</a>
 
         <!-- Ajout du bouton pour télécharger le PDF -->
         <a href="/pdf.php" class="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
