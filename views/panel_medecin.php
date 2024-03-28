31faba3722b2d0d95df6a928d50a2052c7849d33
Kaotiik2 <duribreuxjoshua1@gmail.com>
2024-03-28 15:04:19 +0100

    Update panel_medecin.php

--- views/panel_medecin.php
+++ views/panel_medecin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LPFS - Panel Médecin</title>
     <link rel="stylesheet" href="/static/css/panel_medecin.css">
 </head>
 <body>
